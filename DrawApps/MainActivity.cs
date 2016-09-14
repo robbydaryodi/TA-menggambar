400ab97f526a9ae73ba7f8aa837a123b6c675ca0
ROBBY <ROBBY@ROBBYMAN>
2016-09-14 13:11:23 +0700

    Add project files.

---
+++ DrawApps/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace DrawApps
+{
+    [Activity(Label = "DrawApps", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            DrawCanvas draw = new DrawCanvas(this);
+            draw.start();
+            // Set our view from the "main" layout resource
+            SetContentView(draw);           
+        }
+    }
+}
+
